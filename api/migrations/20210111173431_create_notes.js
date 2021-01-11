fd6a70ab16b35037ed3cfd97bec07ee82c7f12a2
jaredshue <74428951+jaredshue@users.noreply.github.com>
2021-01-11 17:42:08 -0500

    Create 20210111173431_create_notes.js

---
+++ api/migrations/20210111173431_create_notes.js
@@ -0,0 +1,11 @@
+exports.up = function(knex) {
+    return knex.schema.createTable("notes", table => {
+        table.increments("id").notNullable().primary();
+        table.string("note").notNullable();
+        table.timestamp("created_at").defaultTo(knex.fn.now());
+    });
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists("notes");
+};
