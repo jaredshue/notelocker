e32dc98e39e4bc23b4d10f39a4e6bf6a43a573c4
jaredshue <74428951+jaredshue@users.noreply.github.com>
2021-01-11 17:55:58 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,15 @@ function App() {
     return (
         <BrowserRouter>
             <Switch>
+                <Route exact path="">
+                    <Redirect to="/new" />
+                </Route>
+
+                <Route exact path="/new">
+                </Route>
+
+                <Route exact path="/view">
+                </Route>
             </Switch>
         </BrowserRouter>
     );
