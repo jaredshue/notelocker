67c9bcba619f46f70c136de048480ad6612c5469
hilldauk <hilldauk@gmail.com>
2021-01-12 14:10:49 -0600

    Update 20210111173431_create_notes.js

--- api/migrations/20210111173431_create_notes.js
+++ api/migrations/20210111173431_create_notes.js
@@ -1,6 +1,7 @@
 exports.up = function(knex) {
     return knex.schema.createTable("notes", table => {
         table.increments("id").notNullable().primary();
+        table.string("guid").notNullable().unique();
         table.string("note").notNullable();
         table.timestamp("created_at").defaultTo(knex.fn.now());
     });
