fe16af0d574c2d7fca22a75a2a21596d274cf2ce
jaredshue <74428951+jaredshue@users.noreply.github.com>
2021-01-12 17:03:51 -0500

    Fix seeds

--- api/seeds/create_test_seeds.js
+++
@@ -1,9 +0,0 @@
-
-exports.seed = function(knex) {
-    return knex("notes").del()
-        .then(function () {
-            return knex("notes").insert([
-                { guid: "bdc048c0-551d-11eb-8291-7733037ea32b", note: "ae1a824fe857f86a7e7e777c210e2c673abfba3ac8ae3fdd3bbcf51110f5c98c" }
-            ]);
-        });
-};
