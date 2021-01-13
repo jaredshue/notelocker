d4f072ecf5cc592aea0078b0d63a1398fea5dbfa
jaredshue <74428951+jaredshue@users.noreply.github.com>
2021-01-13 17:22:38 -0500

    Update main.js

--- api/src/main.js
+++ api/src/main.js
@@ -19,22 +19,29 @@ const SERVER_LISTEN_PORT = 3001;
 
 app.post("/notes", async (req, res) => {
     var keys = Object.keys(req.body);
-    if (keys.length !== 1 || !keys.includes("note")) {
+    if (keys.length !== 2 || !keys.includes("hash") || !keys.includes("note")) {
         res.status(400).json({ error: "Bad request" });
         return;
     }
 
-    if (typeof (req.body.note) !== "string") {
+    if (typeof(req.body.hash) !== "string" || req.body.hash.length % 2 !== 0) {
         res.status(400).json({ error: "Bad request" });
         return;
     }
 
-    if (req.body.note.length % 2 !== 0) {
+    if (typeof(req.body.note) !== "string" || req.body.note.length % 2 !== 0) {
         res.status(400).json({ error: "Bad request" });
         return;
     }
 
     var validCharacters = "0123456789abcdefABCDEF";
+    for (var i = 0; i < req.body.hash.length; i++) {
+        if (!validCharacters.includes(req.body.hash[i])) {
+            res.status(400).json({ error: "Bad request" });
+            return;
+        }
+    }
+
     for (var i = 0; i < req.body.note.length; i++) {
         if (!validCharacters.includes(req.body.note[i])) {
             res.status(400).json({ error: "Bad request" });
@@ -57,7 +64,7 @@ app.post("/notes", async (req, res) => {
                 break;
             }
 
-            if (i == 4) {
+            if (i === 4) {
                 res.status(500).json({ error: "An unknown problem occured" });
                 return;
             }
@@ -65,6 +72,7 @@ app.post("/notes", async (req, res) => {
 
         await database.into("notes").insert({
             guid: guid,
+            hash: req.body.hash,
             note: req.body.note
         });
 
@@ -94,6 +102,73 @@ app.get("/notes/:guid", async (req, res) => {
     }
 })
 
+app.delete("/notes/:guid", async (req, res) => {
+    var keys = Object.keys(req.body);
+    if (keys.length !== 2 || !keys.includes("guid") || !keys.includes("hash")) {
+        res.status(400).json({ error: "Bad request" });
+        return;
+    }
+
+    if (typeof(req.body.guid) !== "string" || req.body.guid.length % 2 !== 0) {
+        res.status(400).json({ error: "Bad request" });
+        return;
+    }
+
+    if (typeof(req.body.hash) !== "string" || req.body.hash.length % 2 !== 0) {
+        res.status(400).json({ error: "Bad request" });
+        return;
+    }
+
+    var validCharacters = "0123456789abcdefABCDEF-";
+
+    for (var i = 0; i < req.body.guid.length; i++) {
+        if (!validCharacters.includes(req.body.guid[i])) {
+            res.status(400).json({ error: "Bad request" });
+            return;
+        }
+    }
+
+    validCharacters = "0123456789abcdefABCDEF";
+    for (var i = 0; i < req.body.hash.length; i++) {
+        if (!validCharacters.includes(req.body.hash[i])) {
+            res.status(400).json({ error: "Bad request" });
+            return;
+        }
+    }
+
+    try {
+        var notes = await database
+            .select("*")
+            .from("notes")
+            .where("guid", req.body.guid);
+
+        if (notes.length === 0) {
+            res.status(410).json({ error: "Note does not exist" });
+            return;
+        }
+
+        if (notes[0].hash !== req.body.hash) {
+            res.status(422).json({ error: "Incorrect hash" });
+            return;
+        }
+
+        try {
+            await database
+                .from("notes")
+                .where("guid", req.body.guid)
+                .delete();
+        }
+        catch {
+            res.status(500).json({ error: "Failed to delete note from database" });
+            return;
+        }
+
+        res.status(200).json({ success: true });
+    }
+    catch {
+        res.status(500).json({ error: "Failed to retrieve note from the database" });
+    }
+});
 
 app.listen(SERVER_LISTEN_PORT, () => {
     console.log(`Server listening at port ${SERVER_LISTEN_PORT}!`);
