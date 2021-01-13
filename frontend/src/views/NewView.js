fd7294d924391e0727a46567699e80b336552992
jaredshue <74428951+jaredshue@users.noreply.github.com>
2021-01-13 16:20:20 -0500

    Update NewView.js

--- frontend/src/views/NewView.js
+++ frontend/src/views/NewView.js
@@ -1,37 +1,89 @@
-import React, { useState } from "react";
+import React from "react";
 import "./NewView.css";
-import Note from "./Note";
 
+const crypto = require("crypto");
 
 function NewView() {
+    var query = new URLSearchParams(window.location.search);
 
-    const [ password, setPassword ] = useState("");
-    const [ renderSumbit, setRenderSumbit ] = useState(false);
-    const mockPassword = "fjewofj32oijf"
+    const [ state, setState ] = React.useState({
+        note: null,
+        decrypted: false,
+        errors: 0
+    });
+
+    React.useEffect(async () => {
+        if (state.note !== null) return;
+
+        var response = await fetch(`http://localhost:3001/notes/${query.get("guid")}`);
+
+        if (response.status === 200) {
+            var data = await response.json();
+
+            setState({ ...state, note: data });
+        }
+    });
+
+    const readNote = (note, password) => {
+        var salt = crypto.createHash("sha256").update(password).digest();
+        var key = crypto.pbkdf2Sync(password, salt, 256000, 32, "sha256");
+
+        var buffer = Buffer.from(note, "hex");
+        var iv = buffer.slice(0, 16);
+        var data = buffer.slice(16, buffer.length);
+        var cipher = crypto.createDecipheriv("aes-256-cbc", key, iv);
+        var pt = cipher.update(data, "hex", "utf8");
+        pt += cipher.final("utf8");
+
+        return pt;
+    }
+
+    const attemptPassword = async () => {
+        const password = document.getElementById("password").value;
+
+        try {
+            var contents = readNote(state.note.note, password);
+
+            setState({
+                ...state,
+                note: {
+                    ...state.note,
+                    note: contents
+                },
+                decrypted: true,
+                errors: 0
+            });
+        }
+        catch {
+            setState({ ...state, errors: state.errors + 1 });
+        }
+    }
 
     return (
         <>
-            <div className="enter-password">
-                <h1>Password: </h1>
-                <input type="text" onChange={e => setPassword(e.target.value)} placeholder="enter password" ></input>
-                {/* after MVP: toggle password visibility*/}
-                <button onClick={() => { setRenderSumbit(true) }} id="sumbitted-password">Sumbit</button>
-
-            </div>
-
             {
-                // if the password is correct, we will make an GET request from the API
-                renderSumbit === true && password === mockPassword &&
-                <>
-                    <h1>the entered password is: {password} </h1>
-                    <h1>Here is your note: <Note />   </h1>
-                </>
-                // Note we want to get - create_notes.js : ae1a824fe857f86a7e7e777c210e2c673abfba3ac8ae3fdd3bbcf51110f5c98c
-            }
-            {
-                // if the password is wrong
-                renderSumbit === true && password !== mockPassword &&
-                <h1>wrong password</h1>
+                !state.decrypted && state.note !== null
+                ? (
+                    <>
+                        <h2>Password: </h2>
+                        <input type="text" id="password" placeholder="enter password" />
+                        <button onClick={ attemptPassword }>Submit</button>
+                        {
+                            state.errors !== 0
+                            ? (<h1>Wrong password!</h1>)
+                            : (null)
+                        }
+                    </>
+                )
+                : (
+                    <>
+                        {
+                            state.decrypted
+                            ? <p>{ state.note.note }</p>
+                            : <h1>That note does not exist!</h1>
+                        }
+                    </>
+                )
             }
         </>
     );
