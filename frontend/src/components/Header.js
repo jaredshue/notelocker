3bee28cea5a127b49e1928ace4741f30be1fb366
jaredshue <74428951+jaredshue@users.noreply.github.com>
2021-01-14 10:15:12 -0500

    Add semicolons

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React from "react";
 
 function Header() {
     return (
@@ -7,4 +7,4 @@ function Header() {
     );
 }
 
-export default Header
+export default Header;
